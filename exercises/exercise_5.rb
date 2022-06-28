c7652f8fcbb190e3330a3c5ee44d230f4f537f5f
James Booker <98477043+james-booker1@users.noreply.github.com>
2022-06-27 20:50:25 -0700

    adds final code

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+revenue = Store.sum(:annual_revenue)
+puts revenue
+
+average = Store.average(:annual_revenue)
+puts average
+
+high_earners = Store.where("annual_revenue > 1000000").count
+puts high_earners
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,8 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@Store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 10)
+@Store2.employees.create(first_name: "John", last_name: "ewtr", hourly_rate: 60)
+@Store1.employees.create(first_name: "James", last_name: "whoops", hourly_rate: 30)
+@Store2.employees.create(first_name: "Jim", last_name: "food", hourly_rate: 50)
+@Store1.employees.create(first_name: "Jackie", last_name: "farm", hourly_rate: 60)
